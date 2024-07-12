b8415979929ebfffbfdc8e3ea2dc84fea80b7433
Norie <norie.macewan@tiscali.co.uk>
2024-07-12 17:14:15 +0100

    Updated User component to use signal for user state

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const idxRand = Math.floor(Math.random() *DUMMY_USERS.length);
+const idxRand = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[idxRand];
+  selectedUser = signal(DUMMY_USERS[idxRand]);
 
-  get imagePath() { 
-    return  'assets/users/' + this.selectedUser.avatar;
-  }
+  imagePath = computed(() =>'assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
-    const idxRand = Math.floor(Math.random() *DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[idxRand];
+    const idxRand = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[idxRand]);
   }
 }
