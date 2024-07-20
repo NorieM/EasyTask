4abe5cd1757b6eb8b5d97ee93f40e53dca092aa0
Norie <norie.macewan@tiscali.co.uk>
2024-07-20 22:16:25 +0100

    Updated User componet to use Interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User  {
+  id: string;
+  avatar: string;
+  name: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,18 +20,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
-  @Output() select = new EventEmitter();
+  @Input({ required: true }) user!: User;
 
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.user.id)
+    this.select.emit(this.user.id);
   }
 }
